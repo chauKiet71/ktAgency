ada03d383867e9f9e31ca7c395b1cb2f6453ef08
= <=>
2025-04-23 01:30:30 +0700

    edit nane news

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import ScrollToTop from "./pages/Components/ScrollToTop/ScrollToTop"
 import Website from './pages/Service/Website/Website';
 import Abouts from './pages/Abouts/Abouts';
 import Facebook from './pages/Service/Facebook/Facebook';
-import New1 from './pages/News/New1/new1';
+import New1 from './pages/News/New1/New1';
 import New2 from './pages/News/New2/New2';
 
 function App() {
